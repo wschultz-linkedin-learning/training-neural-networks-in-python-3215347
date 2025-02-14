6932d8d82c6ec732e35adb0888b10bb49a5504d9
wschultz-linkedin-learning <wschultz@fultonbank.com>
2025-02-14 15:01:50 +0000

    Updated unit 4 class

--- src/MLP.py
+++ src/MLP.py
@@ -8,9 +8,11 @@ class Perceptron:
 
     def __init__(self, inputs, bias = 1.0):
         """Return a new Perceptron object with the specified number of inputs (+1 for the bias).""" 
-        pass
+        self.weights = (np.random.rand(inputs + 1) * 2) - 1
+        self.bias = bias
 
     def run(self, x):
         """Run the perceptron. x is a python list with the input values."""
-        pass
-        
+        x_sum = np.dot(np.append(x, self.bias), self.weights)
+        return self.sigmoid(x_sum)
+
